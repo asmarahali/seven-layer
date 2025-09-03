c077dc554ce810024889f7356e6cd75955c452f3
Asma <a.rahali@esi-sba.dz>
2025-09-03 22:46:07 +0100

    edit projects section

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
-const repo = "seven-layer"; // your repo name
+const repo = "seven-layer"; 
 
 const nextConfig: NextConfig = {
   output: "export", // enables static export for GitHub Pages
