39a7b4dde0d6e73cb244190488eeae244c3ac79a
asma rahali <126925232+asmarahali@users.noreply.github.com>
2025-09-04 02:31:28 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const repo = "seven-layer"; 
-
 const nextConfig: NextConfig = {
   output: "export", // enables static export for GitHub Pages
   images: { unoptimized: true },
-  basePath: isProd ? `/${repo}` : "",
-  assetPrefix: isProd ? `/${repo}/` : "",
-  trailingSlash: true, // uncommented to support deep-link pages on GitHub Pages
+  trailingSlash: true, // keeps links working in GH Pages
 };
 
 export default nextConfig;
