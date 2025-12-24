9c806f9dc52dc5eb299d658d81764c1a5a89a0b4
Asma <a.rahali@esi-sba.dz>
2025-12-24 23:45:10 +0100

    make some changes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // enables static export for GitHub Pages
   images: { unoptimized: true },
-  trailingSlash: true, // keeps links working in GH Pages
 };
 
 export default nextConfig;
